d32973905f2628f9cfc664b24d015617f5d8cabe
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-09-08 18:50:31 +0600

    first commit

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div className="text-center">
+            <h1>React!</h1>
+        </div>
+    );
+};
+
+export default Countries;
